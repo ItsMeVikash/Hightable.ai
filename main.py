2f4b4c9d0764dc4a61093b91b4b2c3f8657b192a
vikash kumar <vikashlashmi@gmail.com>
2025-02-28 17:24:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+import os
+os.environ["STREAMLIT_THEME_PRIMARY_COLOR"] = "#e2b760"
+os.environ["STREAMLIT_THEME_BACKGROUND_COLOR"] = "#FFFFFF"
+os.environ["STREAMLIT_THEME_SECONDARY_BACKGROUND_COLOR"] = "#f8e8d0"
+os.environ["STREAMLIT_THEME_TEXT_COLOR"] = "#31333F"
+os.environ["STREAMLIT_THEME_FONT"] = "sans serif"
+os.environ["STREAMLIT_THEME_BASE"] = "light"
+
 import streamlit as st
 import ssl
 import json
